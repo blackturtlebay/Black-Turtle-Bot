70bab608a6937f53674c76b3f76f15309e78c192
Black Turtle <blackturtlebay@gmail.com>
2020-06-21 04:57:16 +0000

    mongodb connect success

--- connect.js
+++ connect.js
@@ -1,6 +1,14 @@
-const MongoClient = require('mongodb').MongoClient;
-const mongo_username = process.env.MONGO_USERNAME
-const mongo_password = process.env.MONGO_PASSWORD
-
+const mongo_password = process.env.MONGO_PASSWORD;
+console.log(mongo_password);
+const mongoose = require('mongoose');
 const uri = "mongodb+srv://dbblackturtlebay:${mongo_password}@blackturtlebay-3ni0l.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log("MongoDB Connected…")
+})
+.catch(err => console.log(err));
+
+//this connection works if the password is hardcoded in
--- index.js
+++ index.js
@@ -1,9 +1,26 @@
+require('dotenv').config({ path: require('find-config')('.env') })
+//Discord Bot
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const token = process.env.DiscordBotSec;
-const keep_alive = require('./keep_alive.js');
+//const keep_alive = require('./keep_alive.js');
 const prefix = "/";
 client.login(token);
+//-------------------------------------------//
+//Database Connection
+const mongoose = require('mongoose');
+const uri = process.env.URI;
+console.log(uri);
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log("MongoDB Connected…")
+})
+.catch(err => console.log(err));
+
+
 //-------------------------------------------//
 //Declare Variables
 var message = '';
@@ -14,26 +31,26 @@ var confirmcommand = '';
 var channelid = '';
 var i = 0;
 
+LogPlayerIDs();
 //Custom Commands
-client.on('message', message => {
-  if (message.author === client.user) return;
-  message = message.content;
-  confirmcommand = "/log";
-  messagecheck = message.substring(0, 4);
-  if (messagecheck == confirmmessage) {
-    messageformat = message.replace(/[^0-9]/g, "");
-    messageformat = messageformat.replace(/.{18}/g, '$&,');
-    playerids = messageformat.split(",");  //add all player ids to an array
-    for (i = 0; i < playerids.length; i++) {
-      playerid = playersids[i];
-      
+function LogPlayerIDs() {
+  client.on('message', message => {
+    if (message.author === client.user) return;
+    message = message.content;
+    confirmcommand = "/log";
+    messagecheck = message.substring(0, 4);
+    if (messagecheck == confirmcommand) {
+      messageformat = message.replace(/[^0-9]/g, "");
+      messageformat = messageformat.replace(/.{18}/g, '$&,');
+      playerids = messageformat.split(",");  //add all player ids to an array 
+      for (i = 0; i < playerids.length; i++) {
+        playerid = playerids[i];
+        //db.collection.insertOne({ playerid: "${playerid}" })
+      }
     }
   }
 
 
-
-
-  console.log(channelid);
-});
-
+  )
+};
 
